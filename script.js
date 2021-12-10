9549309fa34650134d929747ea4d8e83a0fd8bfc
Sergio Francisco <spinheiro.lf@gmail.com>
2021-12-10 16:26:56 -0300

    selecionando tarefas

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const button = document.getElementById('criar-tarefa');
 const form = document.getElementById('texto-tarefa');
 const list = document.getElementById('lista-tarefas');
-function getTask(){
-    console.log(form.value);
-    let newTask = document.createElement("li");
-    list.appendChild(newTask);
-    newTask.innerText = form.value;
+const task = document.getElementsByTagName('li');
+function selectTask(event) {
+  event.target.classList.toggle('selected');
+}
 
+function listenAllTask() {
+  for (let i = 0; i < task.length; i += 1) {
+    task[i].addEventListener('click', selectTask);
+    console.log(task[i]);
+  }
+}
+function getTask() {
+  const newTask = document.createElement('li');
+  newTask.className = 'tarefa';
+  list.appendChild(newTask);
+  newTask.innerText = form.value;
+  form.value = '';
+  listenAllTask();
 }
 button.addEventListener('click', getTask);
