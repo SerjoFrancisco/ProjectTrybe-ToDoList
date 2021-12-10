2dcb87064e45ae28933291d05693106436444338
Sergio Francisco <spinheiro.lf@gmail.com>
2021-12-10 17:02:51 -0300

    complete tasks

--- script.js
+++ script.js
@@ -3,15 +3,23 @@ const form = document.getElementById('texto-tarefa');
 const list = document.getElementById('lista-tarefas');
 const task = document.getElementsByTagName('li');
 function selectTask(event) {
+//   if (event.target.classList.contains('selected')) {
+//     event.target.classList.toggle('selected');
+//   } else {
   for (let i = 0; i < task.length; i += 1) {
     task[i].classList.remove('selected');
   }
-  event.target.classList.toggle('selected');
+  event.target.classList.add('selected');
 }
+// }
 
+function doneTask(event) {
+  event.target.classList.toggle('completed');
+}
 function listenAllTask() {
   for (let i = 0; i < task.length; i += 1) {
     task[i].addEventListener('click', selectTask);
+    task[i].addEventListener('dblclick', doneTask);
   }
 }
 function getTask() {
